58c874afb0106dcf7d1cf4296bfcace2b9e537b4
Hoàng Quốc Tuân <hqt95na@gmail.com>
2018-05-02 15:29:56 +0700

    Complete Spring Security Login

--- LibraryManagement/src/main/java/com/dxc/librarymanagement/config/WebSecurityConfig.java
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/config/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
@@ -25,18 +26,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	private DataSource dataSource;
 
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
-		// Sét đặt dịch vụ để tìm kiếm User trong Database.
-		// Và sét đặt PasswordEncoder.
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-
 	}
 
 	@Override
--- LibraryManagement/src/main/java/com/dxc/librarymanagement/service/UserDetailsServiceImpl.java
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/service/UserDetailsServiceImpl.java
@@ -10,42 +10,41 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.dxc.librarymanagement.dao.LibRoleDAO;
-import com.dxc.librarymanagement.dao.TuanDAO;
 import com.dxc.librarymanagement.entities.LibUser;
 import com.dxc.librarymanagement.dao.LibUserDAO;
 
 @Service
 @Transactional
 public class UserDetailsServiceImpl implements UserDetailsService {
-    @Autowired
-    private LibUserDAO libUserDAO;
+	@Autowired
+	private LibUserDAO libUserDAO;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @Override
-    public UserDetails loadUserByUsername(String userName)
-            throws UsernameNotFoundException {
-        LibUser libUser = this.libUserDAO.findByUserName(userName);
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		LibUser libUser = this.libUserDAO.findByUserName(userName);
 
-        if (libUser == null) {
-            System.out.println("User not found! " + userName);
-            throw new UsernameNotFoundException(
-                    "User " + userName + " was not found in the database");
-        }
+		if (libUser == null) {
+			System.out.println("User not found! " + userName);
+			throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+		}
 
-        System.out.println("Found User: " + libUser);
-        // [ROLE_USER, ROLE_ADMIN,..]
-        String roleNames = libUser.getRole().getNameRole();
+		System.out.println("Found User: " + libUser);
+		// [ROLE_USER, ROLE_ADMIN,..]
+		String roleNames = libUser.getRole().getNameRole();
 
-        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
-        GrantedAuthority authority = new SimpleGrantedAuthority(roleNames);
-        grantList.add(authority);
+		List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+		GrantedAuthority authority = new SimpleGrantedAuthority(roleNames);
+		grantList.add(authority);
 
-        UserDetails userDetails = (UserDetails) new User(libUser.getUserName(), //
-                libUser.getPassword(), grantList);
+		UserDetails userDetails = (UserDetails) new User(libUser.getUserName(), //
+				libUser.getPassword().trim(), grantList);
 
-        return userDetails;
-    }
+		return userDetails;
+	}
 }
