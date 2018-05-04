12696a582042410cfc9ef68f410b64973af98661
Luu Dao <dnl101295@gmail.com>
2018-05-04 16:19:15 -0400

    validate Login + add ISBN API

--- LibraryManagement/src/main/resources/static/app/directives/validate.js
+++ LibraryManagement/src/main/resources/static/app/directives/validate.js
@@ -1,18 +1,28 @@
-app.directive("matchPassword", function () {
-    return {
-        require: "ngModel",
-        scope: {
-            otherModelValue: "=matchPassword"
-        },
-        link: function(scope, element, attributes, ngModel) {
-
-            ngModel.$validators.matchPassword = function(modelValue) {
-                return modelValue == scope.otherModelValue;
-            };
-
-            scope.$watch("otherModelValue", function() {
-                ngModel.$validate();
-            });
-        }
-    };
-})
+$(document).ready(function(){
+  $("form[name='registration']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      username: "required",
+      password: {
+        required: true,
+        minlength: 6
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      username: "Please enter your username",
+      password: {
+        required: "Please enter a password",
+        minlength: "Your password must be at least 6 characters long"
+      },
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+        form.submit();
+      }
+  });
+});
