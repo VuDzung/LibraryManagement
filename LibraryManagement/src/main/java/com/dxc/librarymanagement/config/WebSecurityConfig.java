1b17b29a6e8501be3b44ba44d2d6d45f45b83b71
Hoàng Quốc Tuân <hqt95na@gmail.com>
2018-05-04 16:13:19 +0700

    change ShortDescription 128 to max and config login security

--- LibraryManagement/src/main/java/com/dxc/librarymanagement/config/WebSecurityConfig.java
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/config/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		// Trang /userInfo yêu cầu phải login với vai trò ROLE_USER hoặc ROLE_ADMIN.
 		// Nếu chưa login, nó sẽ redirect tới trang /login.
-		http.authorizeRequests().antMatchers("/userInfo").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
+		http.authorizeRequests().antMatchers("/home").access("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')");
 
 		// Trang chỉ dành cho ADMIN
 		http.authorizeRequests().antMatchers("/admin").access("hasRole('ROLE_ADMIN')");
@@ -61,11 +61,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.loginProcessingUrl("/j_spring_security_check") // Submit URL
 				.loginPage("/login")//
 				.defaultSuccessUrl("/home")//
-				.failureUrl("/index?error=true")//
+				.failureUrl("/login?error=true")//
 				.usernameParameter("username")//
 				.passwordParameter("password")
 				// Cấu hình cho Logout Page.
-				.and().logout().logoutUrl("/logout").logoutSuccessUrl("/logoutSuccessful");
+				.and().logout().logoutUrl("/logout").logoutSuccessUrl("/login");
 
 		// Cấu hình Remember Me.
 		http.authorizeRequests().and() //
