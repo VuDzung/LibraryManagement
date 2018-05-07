b4a44320a01732bbdad955c0c28b02a6a2f7823a
Hoàng Quốc Tuân <hqt95na@gmail.com>
2018-05-07 17:27:59 +0700

    add admin page

--- LibraryManagement/src/main/java/com/dxc/librarymanagement/controller/MainController.java
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/controller/MainController.java
@@ -13,68 +13,96 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MainController {
 
-    @RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
-    public String welcomePage(Model model) {
-        model.addAttribute("title", "Welcome");
-        model.addAttribute("message", "This is welcome page!");
-        return "login";
-    }
+	@RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
+	public String welcomePage(Model model) {
+		model.addAttribute("title", "Welcome");
+		model.addAttribute("message", "This is welcome page!");
+		return "login";
+	}
 
-    @RequestMapping(value = "/admin", method = RequestMethod.GET)
-    public String adminPage(Model model, Principal principal) {
+	@RequestMapping(value = { "/ticketmanagement" }, method = RequestMethod.GET)
+	public String welcometicketmanagement(Model model) {
+		model.addAttribute("title", "Welcome");
+		model.addAttribute("message", "This is welcome page!");
+		return "ticketmanagement";
+	}
 
-        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+	@RequestMapping(value = { "/bookmanagement" }, method = RequestMethod.GET)
+	public String welcomebookmanagement(Model model) {
+		model.addAttribute("title", "Welcome");
+		model.addAttribute("message", "This is welcome page!");
+		return "bookmanagement";
+	}
 
-        String userInfo = WebUtils.toString(loginedUser);
-        model.addAttribute("userInfo", userInfo);
+	@RequestMapping(value = { "/reportsTicket" }, method = RequestMethod.GET)
+	public String welcomereportsTicket(Model model) {
+		model.addAttribute("title", "Welcome");
+		model.addAttribute("message", "This is welcome page!");
+		return "reportsTicket";
+	}
 
-        return "adminPage";
-    }
+	@RequestMapping(value = { "/reportsIncome" }, method = RequestMethod.GET)
+	public String welcomereportsIncome(Model model) {
+		model.addAttribute("title", "Welcome");
+		model.addAttribute("message", "This is welcome page!");
+		return "reportsIncome";
+	}
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String loginPage(Model model) {
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage(Model model, Principal principal) {
 
-        return "login";
-    }
+		User loginedUser = (User) ((Authentication) principal).getPrincipal();
 
-//    @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
-//    public String logoutSuccessfulPage(Model model) {
-//        model.addAttribute("title", "Logout");
-//        return "login";
-//    }
+		String userInfo = WebUtils.toString(loginedUser);
+		model.addAttribute("userInfo", userInfo);
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String userInfo(Model model, Principal principal) {
+		return "admin";
+	}
 
-        // Sau khi user login thanh cong se co principal
-        String userName = principal.getName();
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage(Model model) {
 
-        System.out.println("User Name: " + userName);
+		return "login";
+	}
 
-        User loginedUser = (User) ((Authentication) principal).getPrincipal();
+	// @RequestMapping(value = "/logoutSuccessful", method = RequestMethod.GET)
+	// public String logoutSuccessfulPage(Model model) {
+	// model.addAttribute("title", "Logout");
+	// return "login";
+	// }
 
-        String userInfo = WebUtils.toString(loginedUser);
-        model.addAttribute("userInfo", userInfo);
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String userInfo(Model model, Principal principal) {
 
-        return "home";
-    }
+		// Sau khi user login thanh cong se co principal
+		String userName = principal.getName();
 
-    @RequestMapping(value = "/403", method = RequestMethod.GET)
-    public String accessDenied(Model model, Principal principal) {
+		System.out.println("User Name: " + userName);
 
-        if (principal != null) {
-            User loginedUser = (User) ((Authentication) principal).getPrincipal();
+		User loginedUser = (User) ((Authentication) principal).getPrincipal();
 
-            String userInfo = WebUtils.toString(loginedUser);
+		String userInfo = WebUtils.toString(loginedUser);
+		model.addAttribute("userInfo", userInfo);
 
-            model.addAttribute("userInfo", userInfo);
+		return "home";
+	}
 
-            String message = "Hi " + principal.getName() //
-                    + "<br> You do not have permission to access this page!";
-            model.addAttribute("message", message);
+	@RequestMapping(value = "/403", method = RequestMethod.GET)
+	public String accessDenied(Model model, Principal principal) {
 
-        }
+		if (principal != null) {
+			User loginedUser = (User) ((Authentication) principal).getPrincipal();
 
-        return "403Page";
-    }
+			String userInfo = WebUtils.toString(loginedUser);
+
+			model.addAttribute("userInfo", userInfo);
+
+			String message = "Hi " + principal.getName() //
+					+ "<br> You do not have permission to access this page!";
+			model.addAttribute("message", message);
+
+		}
+
+		return "403Page";
+	}
 }
