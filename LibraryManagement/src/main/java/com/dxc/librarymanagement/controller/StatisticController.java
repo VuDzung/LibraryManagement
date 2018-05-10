9b1aae000f53ffee5f9bd518d1b494ea1d3f0781
Bach kutebabe <bachnguyenvan48@gmail.com>
2018-05-10 14:02:16 -0400

    Statistic done by Bach kute

---
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/controller/StatisticController.java
@@ -0,0 +1,38 @@
+package com.dxc.librarymanagement.controller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dxc.librarymanagement.dao.LibBorrowBookDAO;
+
+@RestController
+public class StatisticController {
+	@Autowired
+	private LibBorrowBookDAO borrowdao;
+	
+	@RequestMapping(value= "/home/statistic/{year}")
+	@ResponseBody
+	public List<Map<Integer,Integer>> getMonthly(@PathVariable Integer year) {
+		return borrowdao.getMonthly(year);
+	}
+	
+	@RequestMapping(value= "/home/statistic/yearly")
+	@ResponseBody
+	public List<Map<Integer,Integer>> getYearly() {
+		return borrowdao.getYearly();
+	}
+	
+	@RequestMapping(value= "/home/statistic/{year}/{week}")
+	@ResponseBody
+	public List<Map<Integer,Integer>> getWeekly(@PathVariable Integer week, @PathVariable Integer year) {
+		return borrowdao.getWeekly(week,year);
+	}
+	
+}
--- LibraryManagement/src/main/java/com/dxc/librarymanagement/dao/LibBorrowBookDAO.java
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/dao/LibBorrowBookDAO.java
@@ -1,8 +1,12 @@
 package com.dxc.librarymanagement.dao;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.dxc.librarymanagement.entities.LibBorrowBook;
@@ -10,4 +14,16 @@ import com.dxc.librarymanagement.entities.LibUser;
 @Repository
 public interface LibBorrowBookDAO extends JpaRepository<LibBorrowBook, Integer> {
 	List<LibBorrowBook> findByUser(LibUser user);
+	
+	@Query("select MONTH(DateBorrow) as _month,COUNT(IdBorrow) as borrowed from LibBorrowBook where YEAR(DateBorrow)=:year group by MONTH(DateBorrow)")
+	public List<Map<Integer,Integer>> getMonthly(@Param("year") Integer year);
+	
+	@Query("select year(DateBorrow) as _year,COUNT(IdBorrow) as borrowed from LibBorrowBook group by year(DateBorrow)")
+	public List<Map<Integer,Integer>> getYearly();
+	
+	@Query(value="select datepart(dw, DateBorrow) as _day,COUNT(IdBorrow) as borrowed \r\n" + 
+			"from LibBorrowBook where  DATEPART(WW,DateBorrow)=:week and YEAR(DateBorrow)=:year \r\n" + 
+			"group by datepart(dw, DateBorrow)", nativeQuery = true)
+	public List<Map<Integer,Integer>> getWeekly(@Param("week") Integer week, @Param("year") Integer year);
+
 }
