4083188776de6c89484c5083b250fa2e5a496894
Hoàng Quốc Tuân <hqt95na@gmail.com>
2018-05-10 11:34:43 +0700

    delete some test pages

--- LibraryManagement/src/main/java/com/dxc/librarymanagement/dao/LibIsbnDAO.java
+++ LibraryManagement/src/main/java/com/dxc/librarymanagement/dao/LibIsbnDAO.java
@@ -1,13 +1,10 @@
 package com.dxc.librarymanagement.dao;
 
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dxc.librarymanagement.entities.LibBook;
 import com.dxc.librarymanagement.entities.LibIsbn;
